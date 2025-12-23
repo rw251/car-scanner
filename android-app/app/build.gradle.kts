1c1184bb342c2ed9a3e252103aa19cca1219a7ef
Richard Williams <1234richardwilliams@gmail.com>
2025-12-23 17:18:12 +0000

    Bump version to 1.8

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rw251.pleasecharge"
         minSdk = 33
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `obd-v2.0.27`;
+const CACHE_NAME = `obd-v2.0.28`;
 
 // Never cachs
 const nc = [/microsoft-identity-association/];
