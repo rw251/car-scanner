e1be317ca726bab5d54374f7acfd3621263f676c
Richard Williams <1234richardwilliams@gmail.com>
2025-12-29 19:02:16 +0000

    Bump version to 1.11

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rw251.pleasecharge"
         minSdk = 33
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.10"
+        versionCode = 12
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
